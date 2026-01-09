57fd1023bd2de358692c2eee3ef56f347d17345b
Ivan Sokolskiy <Gladtycoon@gmail.com>
2026-01-09 14:27:52 +0200

    Fixes problems

--- src/utils.py
+++ src/utils.py
@@ -1,41 +1,39 @@
 import json
 import os
 
-from created_classes import Product, Category
+from created_classes import Category, Product
 
 
 def read_json(path: str) -> list[dict]:
-    """ Функция, которая принимает путь до json-файла,
-     читает его и возвращает список словарей """
+    """Функция, которая принимает путь до json-файла,
+    читает его и возвращает список словарей"""
     full_path = os.path.abspath(path)
-    with open(full_path, 'r', encoding='UTF-8') as json_file:
+    with open(full_path, "r", encoding="UTF-8") as json_file:
         data = json.load(json_file)
     return data
 
 
 def create_objects_from_json(data):
-    """ Функция создает объекты Category и Product из данных JSON-файла"""
+    """Функция создает объекты Category и Product из данных JSON-файла"""
     categories = []
     for category_data in data:
         products_list = []
-        for product_data in category_data['products']:
+        for product_data in category_data["products"]:
             product = Product(
-                name=product_data['name'],
-                description=product_data['description'],
-                price=product_data['price'],
-                quantity=product_data['quantity']
+                name=product_data["name"],
+                description=product_data["description"],
+                price=product_data["price"],
+                quantity=product_data["quantity"],
             )
             products_list.append(product)
         category = Category(
-            name=category_data['name'],
-            description=category_data['description'],
-            products=products_list
+            name=category_data["name"], description=category_data["description"], products=products_list
         )
         categories.append(category)
     return categories
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     raw_data = read_json("D:/SkyPro/Home_work_oop/data/products.json")
     created_objects = create_objects_from_json(raw_data)
 
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,20 +1,13 @@
 import pytest
 
-from src.created_classes import Product, Category
+from src.created_classes import Category, Product
+
 
 @pytest.fixture
 def class_product():
-    return Product(
-        name="name",
-        description="description",
-        price="price",
-        quantity="quantity"
-    )
+    return Product(name="name", description="description", price="price", quantity="quantity")
+
 
 @pytest.fixture
 def class_category():
-    return Category(
-        name="name",
-        description="description",
-        products=[]
-    )
+    return Category(name="name", description="description", products=[])
